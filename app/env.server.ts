84608f48b06cc076ab8ff4b9074eea4724950d35
Franklin Shera <fshera96@gmail.com>
2026-01-30 00:30:53 +0300

    update env

--- app/env.server.ts
+++ app/env.server.ts
@@ -43,8 +43,9 @@ const envSchema = z
       .string()
       .default("false")
       .transform((val) => val === "true"),
+    PUBLIC_APP_URL: z.url(),
     BETTER_AUTH_SECRET: z.string().min(1),
-    BETTER_AUTH_URL: z.string().min(1),
+    BETTER_AUTH_URL: z.url(),
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
   })
