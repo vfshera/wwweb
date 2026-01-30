fc9f870613c9dad7b9e2ac4c2c29115f443800d5
Franklin Shera <fshera96@gmail.com>
2026-01-30 03:24:26 +0300

    export auth  fn

--- app/utils/auth-client.ts
+++ app/utils/auth-client.ts
@@ -1,3 +1,12 @@
 import { createAuthClient } from "better-auth/react";
+import { toast } from "sonner";
 
-export const authClient = createAuthClient({});
+export const authClient = createAuthClient({
+  fetchOptions: {
+    onError(context) {
+      toast.error(context.error.message);
+    },
+  },
+});
+
+export const { signIn, signOut, signUp, useSession } = authClient;
