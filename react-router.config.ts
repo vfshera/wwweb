520868cb8b9965e98494639ef0c01797b7080811
Franklin Shera <fshera96@gmail.com>
2026-06-23 17:04:43 +0300

    chore: update packages

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,5 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  ssr: true,
-  future: {
-    v8_middleware: true,
-  },
+  ssr: true,  
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { jsxImagePlugin } from "./vite-plugins/vite-plugin-react-jsx-image";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { reactRouterHonoServer } from "react-router-hono-server/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    jsxImagePlugin(),
-    tailwindcss(),
-    reactRouterHonoServer({ serverEntryPoint: "./server/index.ts" }),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins: [jsxImagePlugin(), tailwindcss(), reactRouter(), tsconfigPaths()],
 });
